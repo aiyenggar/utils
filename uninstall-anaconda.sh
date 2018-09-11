37f53f6f21efd4fc607725d25c5f52691f117e16
Ashwin <aiyenggar@users.noreply.github.com>
2018-09-11 09:01:31 +0530

    getting rid of the 800MB file

--- uninstall-anaconda.sh
+++ uninstall-anaconda.sh
@@ -6,6 +6,9 @@ cd ~
 rm -rf anaconda
 rm -rf .anaconda_backup
 rm -rf .spyder*
+rm -rf .anaconda/navigator
+rm -rf .condarc .conda .continuum
+nano ~/.bash_profile # remove the anaconda directory from your `PATH` env var
 
 cd /Applications
 rm -rf anaconda
