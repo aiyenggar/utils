93274c83412414f1cb071164314fac587f09c069
Ashwin <aiyenggar@users.noreply.github.com>
2019-10-24 10:35:51 +0530

    getting into topic modeling

--- install-conda.sh
+++ install-conda.sh
@@ -1,7 +1,7 @@
-conda update --all
-
 conda install -c conda-forge qgis
 conda install -c conda-forge gdal
+conda install -c anaconda gensim
 
+conda update --all
 conda update conda
 conda update anaconda-navigator
