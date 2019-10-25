551d31f0e43b050879064c0b1131c7c41779f578
Ashwin <aiyenggar@users.noreply.github.com>
2019-10-25 23:22:12 +0530

    visualizing LDA

--- install-conda.sh
+++ install-conda.sh
@@ -1,6 +1,7 @@
 conda install -c conda-forge qgis
 conda install -c conda-forge gdal
 conda install -c anaconda gensim
+conda install -c conda-forge pyldavis
 conda install -c anaconda scikit-learn
 
 conda update --all
