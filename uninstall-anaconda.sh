ada7d251a41f0ffcee70cfd679ddc5a6c94c80b6
Ashwin <aiyenggar@users.noreply.github.com>
2019-10-26 02:52:19 +0530

    updated

--- uninstall-anaconda.sh
+++ uninstall-anaconda.sh
@@ -8,9 +8,11 @@ rm -rf .anaconda_backup
 rm -rf .spyder*
 rm -rf .anaconda/navigator
 rm -rf .condarc .conda .continuum
-nano ~/.bash_profile # remove the anaconda directory from your `PATH` env var
+#nano ~/.bash_profile # remove the anaconda directory from your `PATH` env var
 
+echo "In Applications"
 cd /Applications
+echo "Deleting anaconda"
 rm -rf anaconda
 rm -rf Anaconda-Navigator.app
 
