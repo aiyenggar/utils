94e189b058bdc9c0bca728cb6c01f9d33d5b8f35
Ashwin <aiyenggar@users.noreply.github.com>
2019-12-31 05:37:59 +0530

    geopy

--- conda.sh
+++ conda.sh
@@ -3,6 +3,7 @@ conda install -c conda-forge gdal
 conda install -c anaconda gensim
 conda install -c conda-forge pyldavis
 conda install -c anaconda scikit-learn
+conda install -c conda-forge geopy
 
 conda update --all
 conda update conda
--- installation.sh
+++
@@ -1,7 +0,0 @@
-conda install beautifulsoup4
-conda install urlparse
-conda list | grep url
-pip install -i https://pypi.anaconda.org/pypi/simple uritools
-pip install --upgrade pip
-pip install -i https://pypi.anaconda.org/pypi/simple urllib3
-
